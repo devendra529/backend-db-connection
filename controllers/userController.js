e7dcadc2c7faf2217ead955af6d8931ed3babc10
devendra529 <singhdevendrapratap529@gmail.com>
2026-04-11 22:34:55 +0530

    adding the new page ..logic for routes (controllers)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,37 @@
 //  Logic for routes
+
+const User = require("../models/User");
+
+// @desc    Create new user
+// @route   POST /api/users
+const createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    // Create user in DB
+    const user = await User.create({
+      name,
+      email,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    Get all users
+// @route   GET /api/users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+  getUsers,
+};
