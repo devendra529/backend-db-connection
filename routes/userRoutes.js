064df4c131006c1ce399436c21675d3742bc9544
devendra529 <singhdevendrapratap529@gmail.com>
2026-04-11 22:35:55 +0530

    api routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,18 @@
 //  API routes
+
+const express = require("express");    // Express is a framework used to build backend APIs and web application in node.js
+const router = express.Router();       // A small part of your app that handles specific routes and logic, used to organize your code better
+
+// Import controller functions
+const {
+  createUser,
+  getUsers,
+} = require("../controllers/userController");
+
+// Route for creating user
+router.post("/", createUser);
+
+// Route for getting all users
+router.get("/", getUsers);
+
+module.exports = router;
